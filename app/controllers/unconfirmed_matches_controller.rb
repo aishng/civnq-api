45ad5492ae0d3a85f1d99d1afa0d4ac1937152aa
Tanoy Sinha <tsinha@gmail.com>
2014-11-04 17:06:49 -0500

    attempting to verify match

--- app/controllers/unconfirmed_matches_controller.rb
+++ app/controllers/unconfirmed_matches_controller.rb
@@ -3,17 +3,18 @@ class UnconfirmedMatchesController < ApplicationController
 
   def create
     params.require(:unconfirmed_match).permit(:game, :match_type, :teams, :comment)
-    #params[:unconfirmed_match][:teams] = Team.lookup_player_ids(params[:unconfirmed_match][:teams])
+    params[:unconfirmed_match][:teams] = Team.lookup_player_ids(params[:unconfirmed_match][:teams])
 
-    #if UnconfirmedMatch.validate(params[:unconfirmed_match])
+    if UnconfirmedMatch.validate(params[:unconfirmed_match])
       unconfirmed_match = UnconfirmedMatch.create(game: params[:unconfirmed_match][:game],
                                                   match_type: params[:unconfirmed_match][:match_type],
                                                   teams: params[:unconfirmed_match][:teams],
-                                                  comment: params[:unconfirmed_match][:comment])
+                                                  comment: params[:unconfirmed_match][:comment],
+                                                  reporter_id: params[:unconfirmed_match][:reporter_id])
       render json: unconfirmed_match
-    #else
-      #render json: {}, status: 400
-    #end
+    else
+      render json: {}, status: 400
+    end
   end
 
 
