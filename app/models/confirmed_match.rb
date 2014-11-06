a109b030421d1f90b5854acf8fbc12b299cbd0e4
AshleyGlidden <ashley@ashleyglidden.com>
2014-11-06 10:55:34 -0500

    untracked confirmed matches controller/model/serial

---
+++ app/models/confirmed_match.rb
@@ -0,0 +1,17 @@
+class ConfirmedMatch
+  include Mongoid::Document
+  include Match
+
+  store_in collection: "confirmed_matches"
+
+  module MatchType
+    FFA     = 0
+    Duel    = 1
+    Teamer  = 2
+  end
+
+  module GameType
+    CivV    = 0
+    CivBE   = 1
+  end
+end
